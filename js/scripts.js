6504195d6211045fcc2bd9449afe911f9bffa93f
Kevin Merinsky <kevinmerinsky@gmail.com>
2023-10-12 11:42:28 -0400

    Added info Modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
-import("../js/p5.min.js");
-
 const preview = document.getElementById("preview");
 const Lessons = document.querySelectorAll(".lesson");
+const dialog = document.querySelector("#workshop-description");
+const showButton = document.querySelector(".tooltip > p > button");
+const closeButton = document.querySelector(
+  "#workshop-description > div >button"
+);
 
 Lessons.forEach((Lesson) => {
   Lesson.addEventListener("mouseover", () => {
     preview.src = `./images/${Lesson.dataset.source}`;
   });
+});
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
 });
